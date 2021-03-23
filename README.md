65c7c275f63fd5962860345d7c8aaa9aa863efa7
haardikg <36541643+haardikg@users.noreply.github.com>
2021-03-23 06:54:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-vite-tailwindJIT-starter
+ 
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="text-5xl mb-[1rem]">Welcome to the React + Tailwind + Vite Starter</h1>
+      <p>Made by Haardik Gupta</p>
+    </div>
+  )
+}
+
+export default App
